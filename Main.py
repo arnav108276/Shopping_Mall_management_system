77ac279eb3fe4e572192aedbb60286e55a8b8b33
Saarthakgupta26 <143247640+Saarthakgupta26@users.noreply.github.com>
2023-11-03 14:09:45 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -288,3 +288,93 @@ def bill():
     
     wn.mainloop()
 #Function to take the inputs form the user to generate bill    
+def newCust():    
+    global wn,name1,name2,name3,date,custName
+    #Creating a window
+    wn = tkinter.Tk() 
+    wn.title("PythonGeeks Shop Management System")
+    wn.configure(bg='lavender blush2')
+    wn.minsize(width=500,height=500)
+    wn.geometry("700x600")
+    headingFrame1 = Frame(wn,bg="lavender blush2",bd=5)
+    headingFrame1.place(relx=0.2,rely=0.1,relwidth=0.6,relheight=0.16)
+    headingLabel = Label(headingFrame1, text="New Customer", fg='grey19', font=('Courier',15,'bold'))
+    headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
+    lable1 = Label(wn,text="Date : ", fg='black')
+    lable1.place(relx=0.05,rely=0.3, )
+    #Getting date
+    date = Entry(wn)
+    date.place(relx=0.3,rely=0.3, relwidth=0.62)
+    lable2 = Label(wn,text="Customer Name : ", fg='black')
+    lable2.place(relx=0.05,rely=0.4, )
+    #Getting customer name
+    custName = Entry(wn)
+    custName.place(relx=0.3,rely=0.4, relwidth=0.62)
+    labelFrame = Frame(wn)
+    labelFrame.place(relx=0.1,rely=0.45,relwidth=0.8,relheight=0.4)
+    y = 0.3
+    Label(labelFrame, text="Please enter the quantity of the products you want to buy",font = ('calibri',11,'bold'),
+    fg='black').place(relx=0.07,rely=0.1)
+    Label(labelFrame, text="%-50s%-50s%-30s"%('Product','Price','Quantity'),font = ('calibri',11,'bold'),
+    fg='black').place(relx=0.07,rely=0.2)
+    #Connecting to the database
+    db=mysql.connector.connect(user="root",passwd="root",host="localhost",database='Shop') 
+    cursor=db.cursor()
+    query = 'SELECT * FROM products'
+    cursor.execute(query)
+    res = cursor.fetchall() 
+    print(res)
+    c=1
+    #Showing all the products and creating entries to take the input of the quantity
+    i=res[0]
+    Label(labelFrame,text="%-50s%-50s"%(i[1],i[2]) ,fg='black').place(relx=0.07,rely=y)
+    name1 = Entry(labelFrame)
+    name1.place(relx=0.6,rely=y, relwidth=0.2)
+    y += 0.1
+    i=res[1]
+    Label(labelFrame,text="%-50s%-50s"%(i[1],i[2]) ,fg='black').place(relx=0.07,rely=y)
+    name2 = Entry(labelFrame)
+    name2.place(relx=0.6,rely=y, relwidth=0.2)
+    y += 0.1
+    i=res[2]
+    Label(labelFrame,text="%-50s%-50s"%(i[1],i[2]) ,fg='black').place(relx=0.07,rely=y)
+    name3 = Entry(labelFrame)
+    name3.place(relx=0.6,rely=y, relwidth=0.2)
+    y += 0.1
+     #Button to generate bill
+    Btn= Button(wn,text="Generate Bill",bg='#d1ccc0', fg='black',command=bill)
+    Btn.place(relx=0.28,rely=0.9, relwidth=0.18,relheight=0.08)
+    Quit = Button(wn,text="Quit",bg='#f7f1e3', fg='black', command=wn.destroy)
+    Quit.place(relx=0.55,rely=0.9, relwidth=0.18,relheight=0.08)
+    wn.mainloop()
+#Creating the mail window
+wn = tkinter.Tk() 
+wn.title("PythonGeeks Shop Management System")
+wn.configure(bg='honeydew2')
+wn.minsize(width=500,height=500)
+wn.geometry("700x600")
+headingFrame1 = Frame(wn,bg="snow3",bd=5)
+headingFrame1.place(relx=0.2,rely=0.1,relwidth=0.6,relheight=0.16)
+headingLabel = Label(headingFrame1, text="Welcome to PythonGeeks \n Shop Management System", fg='grey19', font=('Courier',15,'bold'))
+headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
+#Button to add a new product
+btn1 = Button(wn,text="Add a Product",bg='LightBlue1', fg='black', width=20,height=2, command=addProd)
+btn1['font'] = font.Font( size=12)
+btn1.place(x=270,y=175)
+#Button to delete a product
+btn2 = Button(wn,text="Delete a Product",bg='misty rose', fg='black',width=20,height=2,command=delProd)
+btn2['font'] = font.Font( size=12)
+btn2.place(x=270,y=255)
+#Button to view all products
+btn3 = Button(wn,text="View Products",bg='old lace', fg='black',width=20,height=2,command=viewProds)
+btn3['font'] = font.Font( size=12)
+btn3.place(x=270,y=335)
+#Button to add a new sale and generate bill
+btn4 = Button(wn,text="New Customer",bg='lavender blush2', fg='black', width=20,height=2,command = newCust)
+btn4['font'] = font.Font( size=12)
+btn4.place(x=270,y=415)
+wn.mainloop() 
+
+
+
+
