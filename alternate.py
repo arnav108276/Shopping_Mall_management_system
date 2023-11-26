7b6220dc447d113f0fdb211080a0d42166776681
arnav108276 <143831459+arnav108276@users.noreply.github.com>
2023-11-26 15:55:54 +0530

    Update alternate.py

--- alternate.py
+++ alternate.py
@@ -1,30 +1,26 @@
 import tkinter as tk
+import pyttsx3
 from datetime import datetime
-
 class Item:
     def _init_(self, item_id, name, price, quantity):
         self.item_id = item_id
         self.name = name
         self.price = price
         self.quantity = quantity
-
 class ShoppingCart:
     def _init_(self):
         self.items = []
          def add_item(self, item, quantity):
         self.items.append({"item": item, "quantity": quantity})
-
     def calculate_total(self):
         total = 0
         for item in self.items:
             total += item["item"].price * item["quantity"]
         return total
-
 class ShoppingApp:
     def _init_(self, root):
         self.root = root
         self.root.title("Shopping Mall Management System")
-
         self.cart = ShoppingCart()
         self.sales = 0
         self.purchase_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -32,8 +28,6 @@ class ShoppingApp:
             Item(1, "Item A", 10.0, 50),
             Item(2, "Item B", 5.0, 30)
         ]
-
-
 import tkinter as tk
 from datetime import datetime
 class Item:
@@ -105,4 +99,15 @@ if _name_ == "_main_":
     root.mainloop()
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
