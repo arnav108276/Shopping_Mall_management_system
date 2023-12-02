9da3673c5c999a22363e7e0f8c1ff06e7bb1a468
Saarthakgupta26 <143247640+Saarthakgupta26@users.noreply.github.com>
2023-12-02 19:37:17 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -350,8 +350,6 @@ btn1.place(x=370,y=335)
 btn2 = Button(wn,text="Delete a Product",bg='#B6BBC4', fg='black',width=20,height=3,command=delProd)
 btn2['font'] = font.Font( size=16)
 btn2.place(x=670,y=335)
-
-
 #Button to view all products
 btn3 = Button(wn,text="View Products",bg='#B6BBC4', fg='black',width=20,height=3,command=viewProds)
 btn3['font'] = font.Font( size=16)
