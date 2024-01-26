3920112491dd4b829b84e7bede75c3d5f9980f24
Arnav Goel <143831459+arnav108276@users.noreply.github.com>
2024-01-26 09:43:01 +0530

    .

--- Batch_file.bat
+++ Batch_file.bat
@@ -1,2 +1,5 @@
 cd "C:\\Users\\Arnav\\OneDrive\\Documents\\Management"            //Location of file in your pc
 @py "C:\Users\Arnav\OneDrive\Documents\Management\Frontend2.py"       //Path of file in your pc
+
+
+
