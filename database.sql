3a9cbc30073a396b2a11b1944a73d53f6d727de3
Arnav Goel <143831459+arnav108276@users.noreply.github.com>
2024-03-20 10:50:34 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,8 @@
+user=" x ",passwd=" x ",host=" x ",database='Shop'    --  x-→Depend upon user
+  
+-- #Its better to execute below queries before running program
+  
+CREATE DATABASE Shop;
+USE SHOP;   --Entering database
+CREATE TABLE products (date VARCHAR(10),prodName VARCHAR(20), prodPrice VARCHAR(50));
+CREATE TABLE products (date VARCHAR(10),prodName VARCHAR(20), prodPrice VARCHAR(50));
