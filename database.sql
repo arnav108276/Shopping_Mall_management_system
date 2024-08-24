7bfe6faa59b04b71cd7fe110fbb8af18ffe82249
Arnav Goel <143831459+arnav108276@users.noreply.github.com>
2024-08-24 23:38:45 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -3,6 +3,7 @@ user=" x ",passwd=" x ",host=" x ",database='Shop'    --  x-→Depend upon user
 -- #Its better to execute below queries before running program
   
 CREATE DATABASE Shop;
-USE SHOP;   --Entering database
+USE SHOP;                     --Entering database
 CREATE TABLE products (date VARCHAR(10),prodName VARCHAR(20), prodPrice VARCHAR(50));
-CREATE TABLE products (date VARCHAR(10),prodName VARCHAR(20), prodPrice VARCHAR(50));
+
+-- Not running above queries would also run code but may result in errors
